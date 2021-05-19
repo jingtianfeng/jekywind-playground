02506cb558f82e29868c0faf6588b6e39f9ab590
jingtianfeng <jingtianfeng@hotmail.com>
2021-05-19 09:48:42 -0400

    Set up spacing and layout

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,10 +2,23 @@ module.exports = {
     purge: [],
     darkMode: false, // or 'media' or 'class'
     theme: {
-        extend: {},
-    },
-    variants: {
-        extend: {},
+        spacing: {
+            '0': '0px',
+            '1': '5px',
+            '2': '10px',
+            '3': '20px',
+            '4': '40px',
+            '5': '80px',
+            '6': '160px',
+            '7': '320px',
+        },
+        screens: {
+            'md': {'min': '720px'},
+            'lg': {'min': '1280px'},
+        },
+        container: {
+            center: true,
+        },
     },
     plugins: [
         require('@tailwindcss/typography'),
