6d7dc57c51a5915f7a741fa11f1794893c189c72
jingtianfeng <jingtianfeng@hotmail.com>
2021-05-20 00:25:28 -0400

    Tweak font-family and font-size

--- tailwind.config.js
+++ tailwind.config.js
@@ -19,6 +19,39 @@ module.exports = {
         container: {
             center: true,
         },
+        fontFamily: {
+            'sans': ['Inter', 'Helvetica', 'Arial', 'sans-serif'],
+        },
+        fontSize: {
+            'xs': ['0.75rem', {
+                letterSpacing: '0.025em',
+                lineHeight: '1.25',
+            }],
+            'sm': ['0.875rem', {
+                letterSpacing: '0.025em',
+                lineHeight: '1.25',
+            }],
+            'base': ['1rem', {
+                letterSpacing: '0.025em',
+                lineHeight: '1.5',
+            }],
+            'lg': ['1.625rem', {
+                letterSpacing: '0.025em',
+                lineHeight: '1.25',
+            }],
+            'xl': ['2.625rem', {
+                letterSpacing: '0.025em',
+                lineHeight: '1',
+            }],
+            '2xl': ['4.25rem', {
+                letterSpacing: '0.025em',
+                lineHeight: '1',
+            }],
+            '3xl': ['7rem', {
+                letterSpacing: '0.025em',
+                lineHeight: '1',
+            }],
+        }
     },
     plugins: [
         require('@tailwindcss/typography'),
