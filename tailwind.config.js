2fc9521829f6aee0e10eef8de4174c2beebf1725
jingtianfeng <jingtianfeng@hotmail.com>
2021-05-21 05:44:57 -0400

    Install plugins: line-clamp and aspect-ratio

--- tailwind.config.js
+++ tailwind.config.js
@@ -80,5 +80,7 @@ module.exports = {
     plugins: [
         require('@tailwindcss/typography'),
         require('@tailwindcss/forms'),
+        require('@tailwindcss/line-clamp'),
+        require('@tailwindcss/aspect-ratio'),
     ],
 }
